5be05fa00baeb5a0fa2a08b4f5d6250a54a3389b
Grissiom <chaos.proton@gmail.com>
2019-12-23 00:17:56 +0800

    update the doc

--- src/lib.rs
+++ src/lib.rs
@@ -25,6 +25,9 @@ struct Worksheet {
 
 #[pymethods]
 impl Workbook {
+    /// Create a `Workbook` instance from file `path`.
+    ///
+    /// The file type can be `.xls`, `.xlsx`, `.xlsb`, `.xlsm`, `.ods`.
     #[new]
     fn __new__(obj: &PyRawObject, path: String) {
         obj.init({
@@ -34,10 +37,14 @@ impl Workbook {
         });
     }
 
+    /// Get the sheet names of this workbook.
     fn sheet_names(&mut self) -> PyResult<Vec<String>> {
         Ok(self.sheets.sheet_names().to_vec())
     }
 
+    /// Get the `Worksheet` with name.
+    ///
+    /// Exception will be raised if the sheet is not found.
     fn get_sheet(&mut self, name: String, py: Python) -> PyResult<Py<Worksheet>> {
         let range = self.sheets.worksheet_range(name.as_str()).unwrap_or_else(||
             Err(calamine::Error::Msg("sheet not found"))).map_err(to_py_err)?;
@@ -56,18 +63,24 @@ impl Worksheet {
 
 #[pymethods]
 impl Worksheet {
+    /// Get the size of this worksheet in (height, width) format.
     fn get_size(&self) -> PyResult<(u32, u32)> {
         Ok(self._get_size())
     }
 
+    /// Get the width of this worksheet.
     fn width(&self) -> PyResult<u32> {
         Ok(self._get_size().1)
     }
 
+    /// Get the height of this worksheet.
     fn height(&self) -> PyResult<u32> {
         Ok(self._get_size().0)
     }
 
+    /// Get the value in cell(row, col).
+    ///
+    /// If the row and column is beyond the valid area of this sheet, it will return None.
     fn get_value(&self, row: u32, col: u32, py: Python) -> PyResult<pyo3::PyObject> {
         match self.range.get_value((row, col)) {
             None => { Ok(().to_object(py)) }
@@ -110,7 +123,14 @@ impl Worksheet {
 
 // add bindings to the generated python module
 // N.B: names: "librust2py" must be the name of the `.so` or `.pyd` file
-/// This module is implemented in Rust.
+/// Python library to read the Excel files.
+///
+/// Including .xlsx, .xlsb, .xls and .ods files.
+///
+/// This library use [Calamine] as the underlying library and [PyO3] as the wrapper for Python.
+///
+/// [Calamine]: https://github.com/tafia/calamine
+/// [PyO3]: https://github.com/pyo3/pyo3
 #[pymodule]
 fn pyxlsx_rs(_py: Python, m: &PyModule) -> PyResult<()> {
   m.add_class::<Workbook>()?;
